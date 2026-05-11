2df4b0b050867a37d98763a40c22ad1b02155e91
Rutuj <61797632+rutuj24@users.noreply.github.com>
2026-05-11 04:48:01 +0100

    Update tag_categories.js

--- src/data/tag_categories.js
+++ src/data/tag_categories.js
@@ -1,5 +1,17 @@
 const userDefinedTagCategories = {
-	"type": {
-		"description": "type of the paper"
-	}
+	"group": {
+        "description": "The high-level category of the research paper (e.g., Survey, Research)."
+    },
+    "task": {
+        "description": "The specific research task or problem addressed (e.g., Indexing, Semantic Analysis)."
+    },
+    "model": {
+        "description": "The technical model or framework used (e.g., Spark, HDFS, Semantic)."
+    },
+    "optimization": {
+        "description": "The type of performance optimization discussed."
+    },
+    "applications": {
+        "description": "The real-world application domain."
+    }
 }
